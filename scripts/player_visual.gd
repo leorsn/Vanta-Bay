c5d61fc8be0a14b5c641aaa8174da8c003eed4db
leorsn <leo007.rosengruen@gmail.com>
2026-09-03 14:02:30 +0200

    refine humanoid proportions and silhouette

--- scripts/player_visual.gd
+++ scripts/player_visual.gd
@@ -3,6 +3,7 @@ class_name VantaPlayerVisual
 
 var cloth_dark := Color(0.055, 0.06, 0.07, 1.0)
 var cloth_mid := Color(0.12, 0.13, 0.14, 1.0)
+var trouser_color := Color(0.072, 0.078, 0.088, 1.0)
 var shoe_color := Color(0.025, 0.025, 0.028, 1.0)
 var skin_color := Color(0.63, 0.47, 0.36, 1.0)
 
@@ -11,21 +12,23 @@ func _ready() -> void:
     _build_body()
 
 func _build_body() -> void:
-    _mesh_box("Torso", Vector3(0.0, 1.18, 0.0), Vector3(0.62, 0.78, 0.34), cloth_dark, 0.72)
-    _mesh_box("JacketLower", Vector3(0.0, 0.78, 0.0), Vector3(0.54, 0.28, 0.30), cloth_mid, 0.78)
-    _mesh_capsule("Head", Vector3(0.0, 1.92, 0.0), 0.24, 0.50, skin_color, 0.78)
-    _mesh_box("Hair", Vector3(0.0, 2.15, -0.01), Vector3(0.42, 0.15, 0.34), Color(0.025, 0.02, 0.018, 1.0), 0.88)
+    _mesh_box("Torso", Vector3(0.0, 1.22, 0.0), Vector3(0.60, 0.70, 0.34), cloth_dark, 0.72)
+    _mesh_box("JacketLower", Vector3(0.0, 0.84, 0.0), Vector3(0.52, 0.24, 0.30), cloth_mid, 0.78)
+    _mesh_capsule("Neck", Vector3(0.0, 1.62, 0.0), 0.105, 0.22, skin_color, 0.80)
+    _mesh_capsule("Head", Vector3(0.0, 1.94, 0.0), 0.235, 0.48, skin_color, 0.78)
+    _mesh_box("Hair", Vector3(0.0, 2.15, -0.01), Vector3(0.40, 0.13, 0.33), Color(0.025, 0.02, 0.018, 1.0), 0.88)
 
-    _limb("LeftArm", Vector3(-0.40, 1.16, 0.0), Vector3(0.18, 0.74, 0.18), cloth_dark)
-    _limb("RightArm", Vector3(0.40, 1.16, 0.0), Vector3(0.18, 0.74, 0.18), cloth_dark)
-    _limb("LeftLeg", Vector3(-0.17, 0.28, 0.0), Vector3(0.22, 0.90, 0.24), Color(0.075, 0.08, 0.09, 1.0))
-    _limb("RightLeg", Vector3(0.17, 0.28, 0.0), Vector3(0.22, 0.90, 0.24), Color(0.075, 0.08, 0.09, 1.0))
+    _mesh_capsule("LeftUpperArm", Vector3(-0.39, 1.29, 0.0), 0.095, 0.46, cloth_dark, 0.76)
+    _mesh_capsule("RightUpperArm", Vector3(0.39, 1.29, 0.0), 0.095, 0.46, cloth_dark, 0.76)
+    _mesh_capsule("LeftForearm", Vector3(-0.40, 0.94, -0.01), 0.085, 0.40, cloth_mid, 0.78)
+    _mesh_capsule("RightForearm", Vector3(0.40, 0.94, -0.01), 0.085, 0.40, cloth_mid, 0.78)
+    _mesh_capsule("LeftHand", Vector3(-0.40, 0.69, -0.01), 0.075, 0.18, skin_color, 0.82)
+    _mesh_capsule("RightHand", Vector3(0.40, 0.69, -0.01), 0.075, 0.18, skin_color, 0.82)
 
-    _mesh_box("LeftShoe", Vector3(-0.17, -0.18, -0.10), Vector3(0.24, 0.16, 0.46), shoe_color, 0.48)
-    _mesh_box("RightShoe", Vector3(0.17, -0.18, -0.10), Vector3(0.24, 0.16, 0.46), shoe_color, 0.48)
-
-func _limb(node_name: String, position: Vector3, size: Vector3, color: Color) -> void:
-    _mesh_box(node_name, position, size, color, 0.76)
+    _mesh_capsule("LeftLeg", Vector3(-0.16, 0.34, 0.0), 0.11, 0.92, trouser_color, 0.80)
+    _mesh_capsule("RightLeg", Vector3(0.16, 0.34, 0.0), 0.11, 0.92, trouser_color, 0.80)
+    _mesh_box("LeftShoe", Vector3(-0.16, -0.17, -0.09), Vector3(0.24, 0.16, 0.43), shoe_color, 0.48)
+    _mesh_box("RightShoe", Vector3(0.16, -0.17, -0.09), Vector3(0.24, 0.16, 0.43), shoe_color, 0.48)
 
 func _mesh_box(node_name: String, position: Vector3, size: Vector3, color: Color, roughness: float) -> void:
     var node := MeshInstance3D.new()
