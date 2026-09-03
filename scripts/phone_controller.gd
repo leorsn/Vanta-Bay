458e58e6a9a62440f19320d8b07e0722022e18fc
leorsn <leo007.rosengruen@gmail.com>
2026-09-03 12:02:24 +0200

    Add Black Glass storage-device story follow-up

--- scripts/phone_controller.gd
+++ scripts/phone_controller.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 class_name VantaPhoneController
 
 signal mateo_message_read()
+signal black_glass_followup_read()
 
 @onready var panel: Control = $PhonePanel
 @onready var header: Label = $PhonePanel/Header
@@ -11,6 +12,8 @@ signal mateo_message_read()
 var economy: EconomyManager
 var opened := false
 var message_read := false
+var followup_pending := false
+var followup_read := false
 
 func _ready() -> void:
     add_to_group("phone_controller")
@@ -26,9 +29,24 @@ func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed("phone"):
         opened = not opened
         panel.visible = opened
-        if opened and not message_read:
-            message_read = true
-            mateo_message_read.emit()
+        if opened:
+            if followup_pending and not followup_read:
+                followup_read = true
+                black_glass_followup_read.emit()
+            elif not message_read:
+                message_read = true
+                mateo_message_read.emit()
+
+func show_black_glass_followup() -> void:
+    followup_pending = true
+    followup_read = false
+    header.text = "VANTA OS  /  SECURE MESSAGE"
+    content.text = "UNKNOWN DEVICE DETECTED\nEncrypted storage module recovered from the vehicle.\n\nMATEO\nDo not plug that thing into anything.\nBring it when I call.\nAnd Jace... nobody was supposed to know about that car."
+
+func restore_initial_message() -> void:
+    followup_pending = false
+    header.text = "VANTA OS  /  MESSAGES"
+    content.text = "MATEO\nGot something for you.\nGarage off Ocean Drive.\nBring the car to Port Vanta.\nNo questions."
 
 func _on_balances_changed(cash: int, bank: int, rep: int) -> void:
     balance.text = "CASH  $%d\nBANK  $%d\nREP   %d" % [cash, bank, rep]
