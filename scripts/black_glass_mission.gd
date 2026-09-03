265521215c97e173a7feb5dd391efee4cafd6f86
leorsn <leo007.rosengruen@gmail.com>
2026-09-03 11:45:30 +0200

    Gate Black Glass behind Mateo message

--- scripts/black_glass_mission.gd
+++ scripts/black_glass_mission.gd
@@ -6,6 +6,7 @@ class_name BlackGlassMission
 
 var objective_index := 0
 var objectives := [
+    "READ MATEO'S MESSAGE",
     "STEAL THE VEHICLE",
     "WITNESS REPORTING",
     "LOSE THE POLICE",
@@ -16,6 +17,7 @@ var stolen_vehicle: Node3D
 var wanted: WantedManager
 var economy: EconomyManager
 var crime_reporter: CrimeReporter
+var phone: VantaPhoneController
 var mission_label: Label
 
 func _ready() -> void:
@@ -23,18 +25,26 @@ func _ready() -> void:
     wanted = get_tree().get_first_node_in_group("wanted_manager") as WantedManager
     economy = get_tree().get_first_node_in_group("economy_manager") as EconomyManager
     crime_reporter = get_tree().get_first_node_in_group("crime_reporter") as CrimeReporter
+    phone = get_tree().get_first_node_in_group("phone_controller") as VantaPhoneController
     mission_label = get_node_or_null("MissionHUD/Objective") as Label
     if wanted != null:
         wanted.state_changed.connect(_on_wanted_state_changed)
     if crime_reporter != null:
         crime_reporter.report_completed.connect(_on_report_completed)
+    if phone != null:
+        phone.mateo_message_read.connect(_on_mateo_message_read)
     _refresh_hud()
 
+func _on_mateo_message_read() -> void:
+    if objective_index == 0:
+        objective_index = 1
+        _refresh_hud()
+
 func register_vehicle_theft(vehicle: Node3D) -> void:
-    if objective_index != 0:
+    if objective_index != 1:
         return
     stolen_vehicle = vehicle
-    objective_index = 1
+    objective_index = 2
     if crime_reporter != null:
         crime_reporter.report_vehicle_theft(vehicle, 3)
     elif wanted != null:
@@ -43,19 +53,19 @@ func register_vehicle_theft(vehicle: Node3D) -> void:
     _refresh_hud()
 
 func _on_report_completed() -> void:
-    if objective_index == 1:
-        objective_index = 2
-        _refresh_hud()
-
-func _on_wanted_state_changed(state: String, _stars: int) -> void:
-    if objective_index == 2 and state == "ESCAPED":
+    if objective_index == 2:
         objective_index = 3
         _refresh_hud()
 
+func _on_wanted_state_changed(state: String, _stars: int) -> void:
+    if objective_index == 3 and state == "ESCAPED":
+        objective_index = 4
+        _refresh_hud()
+
 func try_deliver(vehicle: Node3D) -> bool:
-    if objective_index != 3 or vehicle != stolen_vehicle:
+    if objective_index != 4 or vehicle != stolen_vehicle:
         return false
-    objective_index = 4
+    objective_index = 5
     if economy != null:
         economy.award_mission("BLACK GLASS", reward_cash, reward_rep)
     _refresh_hud()
@@ -65,7 +75,9 @@ func _refresh_hud() -> void:
     if mission_label == null:
         return
     mission_label.text = "BLACK GLASS\n" + objectives[objective_index]
-    if objective_index == 1:
+    if objective_index == 0:
+        mission_label.text += "\nPRESS P TO OPEN VANTA OS"
+    elif objective_index == 2:
         mission_label.text += "\nA WITNESS IS CALLING POLICE..."
-    elif objective_index == 4:
+    elif objective_index == 5:
         mission_label.text += "\n+$%d BANK   +%d REP" % [reward_cash, reward_rep]
