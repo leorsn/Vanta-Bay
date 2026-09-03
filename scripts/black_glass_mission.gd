218e580a7020d3292f61b69c7ee04ed2c2a415ca
leorsn <leo007.rosengruen@gmail.com>
2026-09-03 11:41:58 +0200

    Post Black Glass rewards into economy

--- scripts/black_glass_mission.gd
+++ scripts/black_glass_mission.gd
@@ -13,11 +13,13 @@ var objectives := [
 ]
 var stolen_vehicle: Node3D
 var wanted: WantedManager
+var economy: EconomyManager
 var mission_label: Label
 
 func _ready() -> void:
     add_to_group("black_glass_mission")
     wanted = get_tree().get_first_node_in_group("wanted_manager") as WantedManager
+    economy = get_tree().get_first_node_in_group("economy_manager") as EconomyManager
     mission_label = get_node_or_null("MissionHUD/Objective") as Label
     if wanted != null:
         wanted.state_changed.connect(_on_wanted_state_changed)
@@ -41,6 +43,8 @@ func try_deliver(vehicle: Node3D) -> bool:
     if objective_index != 2 or vehicle != stolen_vehicle:
         return false
     objective_index = 3
+    if economy != null:
+        economy.award_mission("BLACK GLASS", reward_cash, reward_rep)
     _refresh_hud()
     return true
 
@@ -49,4 +53,4 @@ func _refresh_hud() -> void:
         return
     mission_label.text = "BLACK GLASS\n" + objectives[objective_index]
     if objective_index == 3:
-        mission_label.text += "\n+$%d   +%d REP" % [reward_cash, reward_rep]
+        mission_label.text += "\n+$%d BANK   +%d REP" % [reward_cash, reward_rep]
